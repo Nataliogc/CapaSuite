60ec405af275a17a741ba06e0a65f0951e36fb52
https://github.com/CumbriaSpahotel <comunicaciones@hotelguadiana.es>
2026-09-16 00:16:24 +0200

    Refine lodging concept regex

--- js/segment-analysis.js
+++ js/segment-analysis.js
@@ -103,11 +103,12 @@
                 segment = totalBlock ? null : block.name;
             }
             if (!metric || (!segment && !totalBlock)) continue;
-            const isTotal = /\b(PRO|PROD|PRODUCCIO?N|REVENUE|VENTA|VTA|INGRESOS?)\b/.test(metric);
-            const isLodging = /ALOJAMIENTO|ALOJAM|SUITE|CAMA SUPLETORIA|LATE CHECK OUT|AMPLIACION|RECARGO|\b(DIA|NOCHE|INDIVIDUAL|DOBLE)\b/.test(metric);
+            const isTotal = /\b(PRO|PROD|PRODUCCIO?N|REVENUE|VENTA|VTA|INGRESOS?|TOTAL|TOTALES|NETO|IMPORTE)\b/.test(metric);
+            const isLodging = /HABITACI|ALOJAMIENTO|ALOJAM|SUITE|CAMA SUPLETORIA|LATE CHECK OUT|AMPLIACION|RECARGO|\b(DIA|NOCHE|INDIVIDUAL|DOBLE)\b/.test(metric);
             const isBreakfast = /DESAYUNO|PENSI|BUFFET/.test(metric);
+            const isNonMoney = /PAX|ADULTOS|NIÑOS|BEBES|CUNAS|OCUPACION|PORCENTAJE|%|DIAS|ESTANCIAS|EDAD/.test(metric);
             if (isLodging && !totalBlock) lodgingRows++;
-            if (!isRooms && !isTotal && !isLodging && !isBreakfast) continue;
+            if (isNonMoney) continue;
             if (isRooms && !totalBlock) roomRows++;
             columns.forEach((c, i) => {
                 if (!c) return;
